11fb5548151378d866a313508182d72e96df9f57
Antero Kapunda <alcideskapunda@gmail.com>
2023-10-25 16:15:36 +0100

    Esta terminado a 3ª etapa do desafio

--- js/classificacao.js
+++ js/classificacao.js
@@ -18,5 +18,23 @@ function calcularClassificacao(clubes, pontos) {
     return classificacao;
 
 };
+//console.log(calcularClassificacao(clubes, pontos));
 
-console.log(calcularClassificacao(clubes, pontos));
+// Terceiro desafio
+
+let nInteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function verifica(nInteiros) {
+    const multiplos = [];
+    for (let index = 0; index < nInteiros.length; index++) {
+        if (nInteiros[index] % 2 == 0) {
+            multiplos.push(nInteiros[index]);
+        }
+    }
+
+    multiplos.sort((a, b) => b - a);
+
+    return multiplos
+}
+
+console.log(verifica(nInteiros));
