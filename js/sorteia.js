be73a1f575b75286c5938eeba841c8c6f7a91e75
Antero Kapunda <alcideskapunda@gmail.com>
2023-10-27 13:37:49 +0100

    Alterei uma funcionalidade

--- js/sorteia.js
+++ js/sorteia.js
@@ -5,12 +5,12 @@ for (let index = 0; index < 20; index++) {
     const intervalo = Math.floor(Math.random() * 10);
     numberAleatorio.push(intervalo);
     if (numberAleatorio[index] > 5) {
-        maior.push(numberAleatorio[index]);
+       maior.push(numberAleatorio[index]);
     }
     if (numberAleatorio[index] % 3) {
         divisivel.push(numberAleatorio[index]);
     }
 }
 console.log(`Numeros sorteados ${numberAleatorio}`);
-console.log(`Numeros maiores de 5 => ${maior}`);
-console.log(`Numeros divisiveis por 3 => ${divisivel}`);
+console.log(`Numeros maiores de 5 => ${maior.length}`);
+console.log(`Numeros divisiveis por 3 => ${divisivel.length}`);
