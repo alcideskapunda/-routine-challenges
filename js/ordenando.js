dd3e9b8fac046113a1e946a2cfdbbde3118bae2e
Antero Kapunda <alcideskapunda@gmail.com>
2023-11-12 14:53:44 +0100

    atualização do desafio 08

--- js/ordenando.js
+++ js/ordenando.js
@@ -1,25 +1,39 @@
-let number = [];
+let listaNumeros = [11,4,2,6,4,0,2,0,45,30];
 
-function gerarNumber(number) {
-    for (let index = 1; index <= 10; index++) {
-        number.push(index);
-    }
-    return number;
-}
+function ordenarParaCrescente(conjuntoNumeros) {
+    for (let index = 0; index < conjuntoNumeros.length; index++) {
+        for (let loop = 0; loop < conjuntoNumeros.length; loop++) {
+            if (conjuntoNumeros[index] < conjuntoNumeros[loop]) {
+                // Troca os elementos se estiverem fora de ordem
+                const temp = conjuntoNumeros[index] //variavel temporaria que armazena o valor do conjunto[index]
+                conjuntoNumeros[index] = conjuntoNumeros[loop] 
+                conjuntoNumeros[loop] = temp 
 
-function ordena() {
-    const numberNovo = gerarNumber(number);
-    for (let index = 0; index < numberNovo.length; index++) {
-        if (numberNovo[index] === 1) {
-            numberNovo.length = 0;
-            for (let i = 10; i >= 1; i--) {
-                numberNovo.push(i);
+                // ordenando os elementos usando desestruturação
+                [conjuntoNumeros[index], conjuntoNumeros[loop]] = [conjuntoNumeros[loop], conjuntoNumeros[index]] 
             }
         }
     }
-    return numberNovo;
+    return conjuntoNumeros
 }
 
-console.log(ordena())
- 
+function ordenarParaDecrescente(conjuntoNumeros) {
+    for (let index = 0; index < conjuntoNumeros.length; index++) {
+        for (let loop = 0; loop < conjuntoNumeros.length; loop++) {
+            if (conjuntoNumeros[index] > conjuntoNumeros[loop]) {
+                // ordenando os elementos usando desestruturação
+                [conjuntoNumeros[loop], conjuntoNumeros[index]] = [conjuntoNumeros[index], conjuntoNumeros[loop]] 
+            }
+        }
+    }
+    return conjuntoNumeros
+}
 
+const numerosOrdenados = ordenarParaDecrescente(listaNumeros)
+
+console.log(numerosOrdenados);
+
+// outros testes..................
+let numerosInteros =[10,9,8,7,6,5,4,3,2,1];
+
+console.log(numerosInteros.reverse());
