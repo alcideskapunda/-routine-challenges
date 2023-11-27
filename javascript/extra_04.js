78cf8a4d382b9fda03208f48b0104599ba50788f
Antero Kapunda <alcideskapunda@gmail.com>
2023-11-27 08:26:35 +0100

    chalange extra

---
+++ javascript/extra_04.js
@@ -0,0 +1,27 @@
+/*                                          Nova serie de exercicos - 1
+ Crie uma lista de dicionarios para representar uma agenda de contactos. Cada dicionario deve conter informações como nome, telefone, email de uma pessoa. */
+
+const agenda = [
+    {
+        nome: "Ântero",
+        telefone: 926051630,
+        email: "alcideskapunda22@gmail.com",
+    },
+    {
+        nome: "Bunga",
+        telefone: 953446417,
+        email: "bungalukenene03@gmail.com",
+    },
+];
+
+// Loop forof é como o forEach(só que no forEach vc pode passar dois valores) / depois pesquisar sobre, enquanto ainda não há dominio
+for (const mostra of agenda) {
+    console.log(mostra);
+}
+
+
+// Acho que esta certo mesmo sem nenhuma validação
+
+/*                                          Exercicio nª 2
+Crie umma lista de
+*/
