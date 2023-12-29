3e145680016f40cac0e81b033e0014fe28b4d7d9
Antero Kapunda <alcideskapunda@gmail.com>
2023-12-29 16:05:00 +0100

    ultimo desafio do ano foi terminado

---
+++ js/desafio_10.js
@@ -0,0 +1,27 @@
+// chalenge 10 - o ultimo do ano 2023
+// ler uma matriz 4x4 e calcular do maior com o menor elemento da matriz
+
+const matriz = [[8, 9, 5, 2], [12, 10, 13, 18], [2, 5, 7, 11], [14, 12, 13, 15]];
+
+const soma = (matriz) => {
+    let valoresDaMatrizPercorrida = [];
+    let maiorN;
+    let menorN;
+
+    for (let index = 0; index < matriz.length; index++) {
+        let matrizPercorrida = matriz[index];
+
+        for (let join = 0; join < matrizPercorrida.length; join++) {
+            valoresDaMatrizPercorrida.push(matrizPercorrida[join])
+        }
+    }
+
+    maiorN = Math.max(...valoresDaMatrizPercorrida);
+    menorN = Math.min(...valoresDaMatrizPercorrida);
+
+    return maiorN + menorN;
+}
+
+console.log(soma(matriz));
+
+
