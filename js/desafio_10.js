d88312cd9a5abeacafbd9c27dc018191c90fb637
Antero Kapunda <alcideskapunda@gmail.com>
2023-12-29 17:38:14 +0100

    alteração do metodo de resolução do desafio

--- js/desafio_10.js
+++ js/desafio_10.js
@@ -1,9 +1,10 @@
 // chalenge 10 - o ultimo do ano 2023
-// ler uma matriz 4x4 e calcular a soma do maior com o menor elemento da matriz
+// 1- ler uma matriz 4x4 e calcular a soma do maior com o menor elemento da matriz./
+// 2- não transformar a matriz em array
 
 const matriz = [[8, 9, 5, 2], [12, 10, 13, 18], [2, 5, 7, 11], [14, 12, 13, 15]];
 
-const soma = (matriz) => {
+/* 1- const soma = (matriz) => {
     let valoresDaMatrizPercorrida = [];
     let maiorN;
     let menorN;
@@ -22,6 +23,27 @@ const soma = (matriz) => {
     return maiorN + menorN;
 }
 
-console.log(soma(matriz));
+console.log(soma(matriz)); */
 
+const soma2 = (matriz) => {
+    let maiorN = matriz[0][0];
+    let menorN = matriz[0][0];
 
+    for (let index = 0; index < matriz.length; index++) {
+        let matrizPercorrida = matriz[index];
+
+        for (let join = 0; join < matrizPercorrida.length; join++) {
+            if (matrizPercorrida[join] > maiorN) {
+                maiorN = matrizPercorrida[join]
+            }
+            if (matrizPercorrida[join] < menorN) {
+                menorN = matrizPercorrida[join]
+            }
+        }
+    }
+    let somatorio = maiorN + menorN;
+
+    return somatorio;
+}
+
+console.log(soma2(matriz));
