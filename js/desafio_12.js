1de6e32394484ee3557245805ec9e57de728c879
Antero Kapunda <alcideskapunda@gmail.com>
2024-01-15 16:22:56 +0100

    acho q esta certo

--- js/desafio_12.js
+++ js/desafio_12.js
@@ -1,3 +1,15 @@
+/* 02
+Fazer um programa que recebe um valor número qualquer em milivolts e converte para ppm. */
+let nMilivolts = 99;
+
+const conversaoPpm = (num) => {
+    let fatorConversao = "11000000e-03";
+
+    return fatorConversao * num;
+}
+
+console.log(conversaoPpm(nMilivolts));
+
 // 3- Fazer um programa que permite somar 2 números sem usar o operar de soma(+)
 
 let a = 5;
